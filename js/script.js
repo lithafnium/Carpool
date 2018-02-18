49dbd325961c3de1bbbb5d464edda14d328dc5cb
Steve Li <lithafnium2000@gmail.com>
2018-02-18 09:25:40 -0500

    initialize firebase

---
+++ js/script.js
@@ -0,0 +1,11 @@
+var config = {
+   apiKey: "AIzaSyAY52q8ifdcfv4tgIup_-TqQVcGZ5M7EXs",
+   authDomain: "carpool-3489f.firebaseapp.com",
+   databaseURL: "https://carpool-3489f.firebaseio.com",
+   projectId: "carpool-3489f",
+   storageBucket: "carpool-3489f.appspot.com",
+   messagingSenderId: "450073891207"
+};
+
+firebase.initializeApp(config);
+
